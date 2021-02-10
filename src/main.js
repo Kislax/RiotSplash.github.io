b3e335b01fd2e5558de66443e37c880afd09ff47
Maxim <maxpodstrechny@gmail.com>
2021-02-10 20:46:03 +0200

    add fonts and tag Overhead
    used local fonts Playfair Display, Open Sans Condensed
    tag overhead - title and discription page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import '@riotjs/hot-reload'
 import { component } from 'riot'
-// import registerGlobalComponents from './register-global-components'
 import App from './app.riot'
 
 component(App)(document.getElementById("app"))
 
-// // register
-// registerGlobalComponents()
-//
-// // mount all the global components found in this page
-// mount('[data-riot-component]')
