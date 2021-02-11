7832e3dd37c82f9571ec2fa58903c119ab6c0e85
Kislax <maxpodstrechny@gmail.com>
2021-02-11 17:47:46 +0200

    add req unsplashAPI and render ImageBox

--- src/hooks/unsplash.hook.js
+++ src/hooks/unsplash.hook.js
@@ -1,15 +1,58 @@
-import { createApi }  from 'unsplash-js';
+// import {useState, useCallback} from 'react'
+//
+//
+//
+//
+//
+//
+// export const useHttp = () => {
+//     const [loading, setLoading] = useState(false)
+//     const [error, setError] = useState(null)
+//
+//     const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+//         setLoading(true)
+//         try {
+//             if (body) {
+//                 body = JSON.stringify(body)
+//                 headers['Content-Type'] = 'application/json'
+//             }
+//
+//             const response = await fetch(url, {method, body, headers})
+//             const data = await response.json()
+//
+//             if (!response.ok) {
+//                 throw new Error(data.message || 'Что-то пошло не так')
+//             }
+//
+//             setLoading(false)
+//
+//             return data
+//         } catch (e) {
+//             setLoading(false)
+//             setError(e.message)
+//             throw e
+//         }
+//     }, [])
+//
+//     const clearError = useCallback(() => setError(null), [])
+//
+//     return { loading, request, error, clearError }
+// }
+//
+// //====================================================================//=====
+// const SendSMSHandler = async (phone, command) => {
+//     try {
+//         console.log("sms", phone, command)
+//         const smsPush = await request('/api/wialon/sendSMS', 'post', {phone, command}, {
+//             Authorization: `Bearer ${token}`
+//         })
+//         message(smsPush.message)
+//         console.log(smsPush)
+//         setDelivSMS(smsPush)
+//     } catch (e) {
+//     }
+// }
 
-const unsplash = createApi({ accessKey: 'AFDhwv8KgCPMlQv3LwMNtoGBh10JcgxBvEcZ-G3Jc8w' });
 
-// non-feed example
-unsplash.photos.get({ photoId: 'foo' }).then(result => {
-    if (result.errors) {
-        // handle error here
-        console.log('error occurred: ', result.errors[0]);
-    } else {
-        // handle success here
-        const photo = result.response;
-        console.log(photo);
-    }
-});
+fetch('https://api.unsplash.com/photos/?client_id=AFDhwv8KgCPMlQv3LwMNtoGBh10JcgxBvEcZ-G3Jc8w').then(response => response.json())
+        .then(commits => console.log(commits[0]));
